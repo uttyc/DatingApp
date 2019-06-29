6a59bb9dc5169d85604a6f02d9075554dbecd64f
uttyc <atikcandurmus@gmail.com>
2019-06-29 13:22:51 +0300

    Database Update

--- DatingApp.API/Models/Data/DataContext.cs
+++ DatingApp.API/Models/Data/DataContext.cs
@@ -11,6 +11,7 @@ namespace DatingApp.API.Models.Data
 
         public DataContext(DbContextOptions<DataContext> options) : base(options){}
         public DbSet<Value> Values { get; set; }
-        
+   
+        public DbSet<User> Users { get; set; }
     }
 }
