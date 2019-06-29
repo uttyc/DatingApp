45c85956d044fd4000091c9ec20b3cd4aae3e783
uttyc <atikcandurmus@gmail.com>
2019-06-29 13:29:55 +0300

    Db update

--- DatingApp.API/Migrations/20190629101606_AddedUserEntity.cs
+++ DatingApp.API/Migrations/20190629101606_AddedUserEntity.cs
@@ -6,22 +6,22 @@ namespace DatingApp.API.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
- migrationBuilder.CreateTable(
-                name: "Users",
-                columns: table => new
-                {
-                    Id = table.Column<int>(nullable: false)
-                        .Annotation("Sqlite:Autoincrement", true),
-                    Username = table.Column<string>(nullable: true),
-                    
-                    PasswordHash = table.Column<byte[]>(nullable: true),
-                    
-                    PasswordSalt = table.Column<byte[]>(nullable: true)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Users", x => x.Id);
-                });
+            migrationBuilder.CreateTable(
+                           name: "Users",
+                           columns: table => new
+                           {
+                               Id = table.Column<int>(nullable: false)
+                                   .Annotation("Sqlite:Autoincrement", true),
+                               Username = table.Column<string>(nullable: true),
+
+                               PasswordHash = table.Column<byte[]>(nullable: true),
+
+                               PasswordSalt = table.Column<byte[]>(nullable: true)
+                           },
+                           constraints: table =>
+                           {
+                               table.PrimaryKey("PK_Users", x => x.Id);
+                           });
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
